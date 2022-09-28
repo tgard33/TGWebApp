0ceb4cdf157378fcdd259a3d6726b7a776a5b370
Tyler Gardner <tyler.gardner@maersk.com>
2022-09-28 12:17:58 -0600

    Add project files.

---
+++ Pages/College.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TGWebApp.Pages
+{
+    public class CollegeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Mission.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TGWebApp.Pages
+{
+    public class MissionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
